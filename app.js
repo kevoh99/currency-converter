a63623bced4aaff2180abd41ce8d07bdbbc5ab9b
Kevin Njoroge <njorogekevin.njoroge@gmail.com>
2019-04-17 19:33:16 +0300

    Added objects as function arguments

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-/* Next, try passing in objects as function arguments as opposed to multiple variables. Useful URL: https://stackoverflow.com/questions/7764536/pass-object-to-javascript-function */
-
 const axios = require('axios')
 
-const getExchangeRate = async (fromCurrency, toCurrency) => {
+// const getExchangeRate = async (fromCurrency, toCurrency) => {
+const getExchangeRate = async (currencyData) => {
   try {
-    const defaultCurrency = 'USD' // According to Apilayer API documentation
-    const sourceCurrency = `${defaultCurrency}${fromCurrency}` // Example USDKES
-    const resultingCurrency = `${defaultCurrency}${toCurrency}` // Example USDHRK
+    const defaultCurrency = 'USD' // According to CurrencyLayer API documentation
+    const sourceCurrency = `${defaultCurrency}${currencyData.fromCurrency}` // Example USDKES
+    const resultingCurrency = `${defaultCurrency}${currencyData.toCurrency}` // Example USDHRK
     const response = await axios.get('http://www.apilayer.net/api/live?access_key=af908b96fc9d56f13d11929909a54b1c&format=1')
     const rate = response['data']['quotes'][sourceCurrency] // Example "USDKES":100.9445
     const dollarRate = 1 / rate // 1 / 100.9445 = 0.0099064
@@ -14,7 +13,7 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
     const exchangeRate = dollarRate * conversionCurrencyRate // 0.0099064 * 6.594197
     return exchangeRate
   } catch (error) {
-    throw new Error(`Unable to get currency data using ${fromCurrency} and ${toCurrency}. Please try again. If the problem persists, please contact us`)
+    throw new Error(`Unable to get currency data using ${currencyData.fromCurrency} and ${currencyData.toCurrency}. Please try again. If the problem persists, please contact us`)
   }
 }
 
@@ -31,14 +30,25 @@ const getCountries = async (currencyCode) => {
   }
 }
 
-const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-  const exchangeRate = await getExchangeRate(fromCurrency, toCurrency)
-  const convertedAmount = (amount * exchangeRate).toFixed(2)
-  const countries = await getCountries(toCurrency)
-  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend the cash in the following countries: ${countries}`
+// const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+const convertCurrency = async (currencyData) => {
+  let data = {
+    fromCurrency: currencyData.fromCurrency,
+    toCurrency: currencyData.toCurrency
+  }
+  const exchangeRate = await getExchangeRate(data)
+
+  const convertedAmount = (currencyData.amount * exchangeRate).toFixed(2)
+  const countries = await getCountries(currencyData.toCurrency)
+  return `${currencyData.amount} ${currencyData.fromCurrency} is worth ${convertedAmount} ${currencyData.toCurrency}. You can spend the cash in the following countries: ${countries}`
 }
 
-convertCurrency('KES', 'USD', 100000)
+let currencyData = {
+  fromCurrency: 'USD', // on real website, this would come from user input in the form
+  toCurrency: 'GBP', // on real website, this would come from user input in the form
+  amount: 10000 // on real website, this would come from user input in the form
+}
+convertCurrency(currencyData)
   .then((message) => {
     console.log(message)
   }).catch((error) => {
